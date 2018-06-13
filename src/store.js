533bec03bbbdc772812a2f4f8a42acd63d9d38a0
Obykhvost Ivan <iv@yourserveradmin.com>
2018-06-13 14:53:31 +0300

    Create store

--- src/store.js
+++ src/store.js
@@ -1,1 +1,24 @@
-﻿
+﻿import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+//add reducer
+//import { } from 
+
+const enhancers = [];
+enhancers.push(window.devToolsExtension ? window.devToolsExtension() : f => f);
+
+const middleware = [
+    thunk
+]
+
+const composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+)
+
+const reducer = combineReducers({});
+
+export default createStore(
+    reducer,
+    composedEnhancers
+)
