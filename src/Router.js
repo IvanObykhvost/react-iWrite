1d1e3998a7f1061278c19ccb7535f757401d624c
Obykhvost Ivan <iv@yourserveradmin.com>
2018-06-18 11:42:43 +0300

    Add HomePage and style

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,8 @@
 ﻿import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { compose } from 'redux';
-import HomePage from './scenes/Home/index';
+import HomePage from './scenes/Home/HomePage';
 import PostNew from './containers/Post/PostNew';
-import App from './App';
 
 export default function Router() {
     return <BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+
+import registerServiceWorker from './registerServiceWorker';
 import store from './store';
+import Router from './Router';
 
+import './index.css';
+import './main.css';
+import 'bootstrap/dist/css/bootstrap.css'; 
 
+function Root({ children }) {
+    return <div id="App">
+        {children}
+    </div>;
+}
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <Root>
+            <Router />
+        </Root>
     </Provider>
     ,document.getElementById('root'));
 registerServiceWorker();
